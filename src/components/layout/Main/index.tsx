10a1327dd6a1c66cea5a0317ab38d8b321eafaec
Northa Huang <sonic800122@gmail.com>
2020-10-07 06:57:51 +0800

    Split router data to routes.tsx

--- src/components/layout/Main/index.tsx
+++ src/components/layout/Main/index.tsx
@@ -2,11 +2,9 @@ import React, { useContext } from 'react';
 import StyledWrapper from './styled';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import context from '../../../store/context';
-import Home from '../../Home';
-import SearchResult from '../../SearchResult';
-import ItemDetail from '../../ItemDetail';
-import Loading from '../../Loading';
+import routes from '../../../routes';
 import Error from '../../Error';
+import Loading from '../../Loading';
 
 const Main = () => {
   const { isLoading } = useContext(context);
@@ -15,18 +13,9 @@ const Main = () => {
     <StyledWrapper>
       <Router>
         <Switch>
-          <Route path="/result">
-            <SearchResult />
-          </Route>
-          <Route path="/detail/:id">
-            <ItemDetail />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/error">
-            <Error />
-          </Route>
+          {routes.map(({ name, path, component, exact }) => (
+            <Route key={name} exact={exact} path={path} component={component} />
+          ))}
           <Route>
             <Error message="404 PAGE NOT FOUND." />
           </Route>
